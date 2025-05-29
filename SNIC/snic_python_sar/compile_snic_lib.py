ceabf2789f6136be38147875292a248cfd8be7f5
Omowonuola Akintola <omowonuolaakintola@Omowonuolas-MBP.lan>
2025-05-29 11:19:22 +0200

    update path for compile file for binder

--- SNIC/snic_python_sar/compile_snic_lib.py
+++ SNIC/snic_python_sar/compile_snic_lib.py
@@ -1,22 +1,29 @@
 
 from cffi import FFI
+import os
 
 def compile_sniclib():
-	ffibuilder = FFI()
+    ffibuilder = FFI()
+    
+    # Get directory of this script
+    base_dir = os.path.dirname(os.path.abspath(__file__))
 
-	ffibuilder.cdef(open("snic.h").read())
+    # Absolute paths to files
+    h_path = os.path.join(base_dir, "snic.h")
+    c_path = os.path.join(base_dir, "snic.c")
 
-	ffibuilder.set_source(
-		"_snic",
-		r"""
-			#include "snic.h"
-		""",
-		sources=["snic.c"],
-		library_dirs=['.'],
-		extra_compile_args=['-O3', '-march=native', '-ffast-math'])
+    # Load the header
+    ffibuilder.cdef(open(h_path).read())
 
-	ffibuilder.compile(verbose=True)
+    # Set the source using include
+    ffibuilder.set_source(
+        "_snic",
+        '#include "snic.h"',
+        sources=[c_path],
+        library_dirs=[base_dir],
+        extra_compile_args=['-O3', '-march=native', '-ffast-math'])
 
-
+    ffibuilder.compile(verbose=True)
+    
 
 compile_sniclib()
